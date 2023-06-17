3301ce97c983d3c45571a1f45d5a2cd79d06077f
Connor Hartland <connorhartland@gmail.com>
2023-06-16 18:57:45 -0700

    db connecting

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,16 @@
 import express from 'express'
+import { connectToDB } from './db'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
+connectToDB()
+
 app.get('/', (req, res) => {
+  console.log(req.body)
   res.send('Hello World!')
 })
 
