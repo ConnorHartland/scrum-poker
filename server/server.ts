63ce5988dc926b8151e650d6143076620b4425e2
Connor Hartland <connorhartland@gmail.com>
2023-06-16 18:43:55 -0700

    basics

--- server/server.ts
+++ server/server.ts
@@ -1,27 +1,12 @@
-import 'dotenv/config'
 import express from 'express'
-import cors from 'cors'
-import morgan from 'morgan'
-import { json } from 'body-parser'
-import mongoose from 'mongoose'
-import connectDB from './config/database.ts'
 
 const app = express()
 const PORT = process.env.PORT || 5000
-const errorHandler = require('./middleware/error')
 
-connectDB()
-
-app.use(express.json())
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/private', require('./routes/private'))
-
-app.use(errorHandler)
-
-const server = app.listen(PORT, () => {
-  console.log(`server us running on port ${PORT}`)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
-process.on('unhandledRejection', (error, promise) => {
-  console.log(`Logged Error: ${error}`)
-  server.close(() => process.exit(1))
+
+app.listen(PORT, () => {
+  console.log(`Server is running in http://localhost:${PORT}`)
 })
